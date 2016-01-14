b3b27834a6479e0fe3c6f0d60465f762c721a78c
Nic Schumann <nicolas_schumann@brown.edu>
2016-01-13 19:10:41 -0500

    added window reactivity to Main [w/ ruben]

--- Main.elm
+++ Main.elm
@@ -1,58 +1,120 @@
 
-import Time exposing ( Time, every, second, timestamp )
+import Time exposing ( Time, every, second, minute, timestamp )
 
 import Mouse
+import Window
 
-import Signal exposing (Signal, map, merge, foldp)
+import Signal exposing (Signal, map, mergeMany, foldp)
+import Signal.Extra exposing (foldp')
 
 import Html exposing ( Html )
+import Svg exposing (svg)
+import Svg.Attributes exposing (width, height)
 
 import Loop.Disk exposing (Disk)
 
 import Loop.RandomDisk as Disk
 
-import Loop.Views.Linear as Linear
+import Loop.Views.Radial as Radial
+import Loop.Views.Render as Render
 
+type alias Width = Int
 
-type alias Model = Disk.RandomDisk
+type alias Height = Int
+
+type alias Model = (Disk.RandomDisk, (Width,Height))
+
+type Update = Tick Time | Reset Time | Resize Time (Width,Height)
 
 radial : Int
-radial = 10
+radial = 20
 
 concentric : Int
-concentric = 15
+concentric = 20
 
-
-type Update = Tick Time | Reset Time
-
+initial : (Width,Height)
+initial = (1000,1000)
 
 feed : Signal Update
 feed =
-    merge 
-        (map Reset (map fst (timestamp Mouse.clicks)))
-        (map Tick (every (second / 12)))
+    mergeMany 
+        [ map (\(t,d) -> Resize t d) (timestamp Window.dimensions)
+        , (map Reset << map fst) (timestamp Mouse.clicks)
+        , map Tick (every (minute / 136))
+        ]
 
 
 update : Signal Model
 update =
-    foldp step (Disk.initial 400 (radial,concentric)) feed
+    let
+        buildInitial update =
+            case update of 
+                Tick t -> 
+                    (Disk.initial t (radial,concentric), initial) 
+
+                Reset t ->
+                    (Disk.initial t (radial,concentric), initial) 
+
+                Resize t dim ->
+                    (Disk.initial t (radial,concentric), dim) 
+
+    in
+        foldp' buildStep buildInitial feed
 
 
-step : Update -> Model -> Model
-step update randomDisk =
+
+buildStep : Update -> Model -> Model
+buildStep update (randomDisk, (w,h)) =
     case update of 
         Tick t ->
-            Disk.update randomDisk
+            let 
+                new = Disk.update randomDisk
+
+            in 
+                if .loop (fst new) /= .loop (fst randomDisk) then
+
+                   (new, (w,h))
+
+                else 
+
+                    buildStep (Reset t) (randomDisk,(w,h))
+
+            
 
         Reset t ->
-            Disk.initial t (radial,concentric)
+            (Disk.initial t (radial,concentric), (w,h))
+
+        Resize t dim ->
+            (randomDisk, dim)
+
 
 
 render : Model -> Html
-render (disk,seed) =
-    Linear.view (500,500) 300 disk
+render ((disk,seed),(wI,hI)) =
+    let 
+        w = toFloat wI 
+        h = toFloat hI
+    in
+        svg [width (toString w), height (toString h)] [Render.view Radial.draw (w/2.0,h/2.0) ((min w h)/2.0) disk]
 
 
 main : Signal Html
 main =
+
     map render update
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
